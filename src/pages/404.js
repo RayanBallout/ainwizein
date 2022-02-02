8f48cb32f413e50819b62c1e32e42889b4dbc68e
Rayan Ballout <rayanballout14@gmail.com>
2022-02-02 20:15:48 +0200

    Adding the 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MedicalVillageLayout from '../components/Layout/medical-village';
+
+const GeriatricHome = () => {
+    return (
+        <MedicalVillageLayout>
+            404
+        </MedicalVillageLayout>
+    );
+};
+
+export default GeriatricHome
